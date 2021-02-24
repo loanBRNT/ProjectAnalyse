ff729bade60fad7112d3807ccb9a9efa83d04bac
BERNAT Loan <loantravail@gmail.com>
2021-02-24 11:09:22 +0100

    debut init f

--- BernatGaudillat.py
+++ BernatGaudillat.py
@@ -1,3 +1,5 @@
 # Créé par loant, le 21/02/2021 en Python 3.7
 
 # Projet analyse de donnée
+
+
---
+++ initf.py
@@ -0,0 +1,13 @@
+# Créé par loant, le 24/02/2021 en Python 3.7
+
+#brouillon pour initialiser f
+
+import numpy as np
+from scipy.sparse import csr_matrix
+
+e1 = np.array([1,0,0,0]);
+e2 = np.array([0,1,0,0]);
+e3 = np.array([0,0,1,0]);
+e4 = np.array([0,0,0,1]);
+
+print(e1);
